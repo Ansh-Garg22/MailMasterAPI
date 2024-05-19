bd94c4d6b696a23d6fdcd63af22992ed8c82adb8
Ansh-Garg22 <connect2anshgarg@gmail.com>
2024-05-19 14:38:04 +0530

    UPD

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.get('/', (req, res) => {
+  res.send("---");
+});
 app.use("/lists", listRoutes);
 app.use("/users", userRoutes);
 app.use("/emails", emailRoutes);
